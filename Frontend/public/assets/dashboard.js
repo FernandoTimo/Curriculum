157367e3a2beaf344c20e101c0d9d720d0542363
FernandoTimo <Fertimo99@gmail.com>
2021-02-16 15:54:20 -0500

    Dashboard

--- Frontend/public/assets/dashboard.js
+++ Frontend/public/assets/dashboard.js
@@ -1,15 +1,16 @@
-import styleDashboard from 'styles/css/Dashboard.module.css';
+// import styleDashboard from 'styles/css/Dashboard.module.css';
 import { Header_Main } from 'heads/Header_Main';
-import { Section, Body } from 'components/Resources/Timoideas';
+import { Section, Body, Content } from 'components/Resources/Timoideas';
 export default function Dashboard() {
   return (
     <>
       <Header_Main />
-      <Body>
+      Hola
+      {/* <Body>
         <Section size={1}>
           <h1>Dashboard</h1>
         </Section>
-      </Body>
+      </Body> */}
     </>
   );
 }
