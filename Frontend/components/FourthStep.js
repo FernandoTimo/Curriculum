ace626616c688502001fbb1213043ae7255772bc
FernandoTimo <Fertimo99@gmail.com>
2021-02-17 21:19:50 -0500

    Complementos + ModaÃl

--- Frontend/components/FourthStep.js
+++ Frontend/components/FourthStep.js
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import style from './FourthStep.module.css';
+import { Modal, Poligon } from './Resources/Timoideas';
 function FourthStep() {
+  const [ModalActive, setModalActive] = useState(false);
+  const handlerFunction = () => {
+    setModalActive(false);
+  };
   return (
     <div className={style.FourthStep}>
+      <Modal
+        background={'#0003'}
+        center
+        active={[ModalActive, handlerFunction]}
+        blur={1}
+        transition={0.2}
+      >
+        <Poligon size={'50vh'}>Hola</Poligon>
+      </Modal>
       <h2>Complementos</h2>
-      <div className={style.Container}>
-        <div>
-          <div className={style.outter}>
-            <div className={style.inner}>
-              <div className={style.bg}></div>
-            </div>
+      <Poligon size={'50vh'}>
+        <Poligon size={'45vh'}>
+          <div
+            className={style.Into}
+            onClick={() => {
+              setModalActive(true);
+            }}
+          >
+            Hola
           </div>
-        </div>
-      </div>
+        </Poligon>
+      </Poligon>
     </div>
   );
 }
