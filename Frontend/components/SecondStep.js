e75ea5dd4f83f68188d162131ec0a002d6645642
FernandoTimo <Fertimo99@gmail.com>
2021-02-18 14:07:31 -0500

    Bosquejado

--- Frontend/components/SecondStep.js
+++ Frontend/components/SecondStep.js
@@ -1,19 +1,22 @@
+import { useState } from 'react';
+import { Modal } from './Resources/Timoideas';
 import style from './SecondStep.module.css';
 function SecondStep() {
+  const [ModalProyectos, setModalProyectos] = useState(true);
+  const toggleModalProyectos = () => {
+    setModalProyectos(false);
+  };
   return (
     <div className={style.SecondStep}>
       <Modal
-        background={'#fff3'}
+        background={'#0000'}
         center
-        active={[
-          true,
-          () => {
-            console.log('desactived');
-          },
-        ]}
+        active={[ModalProyectos, toggleModalProyectos]}
         blur={1}
         transition={0.2}
-      ></Modal>
+      >
+        Proyectos
+      </Modal>
       <div className={style.Title}>
         <h2>Stack</h2>
         <h2>Stack</h2>
@@ -21,10 +24,22 @@ function SecondStep() {
       </div>
       <div className={style.StackContainer}>
         <div className={style.Card}>
-          <label>Frontend</label>
+          <div>Librería: React</div>
+          <div>Framework: Next</div>
+          <div>Deploy: Vercel</div>
         </div>
+        <button
+          onClick={() => {
+            setModalProyectos(true);
+          }}
+        >
+          Ver Proyectos
+        </button>
         <div className={style.Card}>
-          <label>Backend</label>
+          <div>Entorno: Node</div>
+          <div>Framework: Express</div>
+          <div>Base de Datos: MongoDB </div>
+          <div>Deploy: Heroku - MongoDB Atlas</div>
         </div>
       </div>
     </div>
