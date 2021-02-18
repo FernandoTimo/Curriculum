f76b266fe96688efc01c6085405c3184856b1188
FernandoTimo <Fertimo99@gmail.com>
2021-02-18 11:55:44 -0500

    Title, cards, Modal

--- Frontend/components/SecondStep.js
+++ Frontend/components/SecondStep.js
@@ -2,7 +2,31 @@ import style from './SecondStep.module.css';
 function SecondStep() {
   return (
     <div className={style.SecondStep}>
-      <h2>SecondStep</h2>
+      <Modal
+        background={'#fff3'}
+        center
+        active={[
+          true,
+          () => {
+            console.log('desactived');
+          },
+        ]}
+        blur={1}
+        transition={0.2}
+      ></Modal>
+      <div className={style.Title}>
+        <h2>Stack</h2>
+        <h2>Stack</h2>
+        <h2>Stack</h2>
+      </div>
+      <div className={style.StackContainer}>
+        <div className={style.Card}>
+          <label>Frontend</label>
+        </div>
+        <div className={style.Card}>
+          <label>Backend</label>
+        </div>
+      </div>
     </div>
   );
 }
